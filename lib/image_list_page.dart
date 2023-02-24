e45fbe6e8c87dc7d6ae223d834cccaa74e6bfea1
MLLB <84686926+AppDeveloperMLLB@users.noreply.github.com>
2023-02-24 10:22:55 +0900

    画像一覧ページの実装 (#3)
    * feat/implement_image_list_page first commit
    
    * グリッドビューで画像一覧を表示するように実装
    
    ---------
    
    Co-authored-by: MLLB <app.developer.mllb@gmail.com>

--- lib/image_list_page.dart
+++ lib/image_list_page.dart
@@ -19,11 +19,44 @@ class ImageListPage extends StatelessWidget {
           "ImageListPage",
         ),
       ),
-      body: const SafeArea(
+      body: SafeArea(
         child: Center(
-          child: Text(
-            "ImageListPage",
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8,
+              crossAxisCount: 3,
+            ),
+            itemCount: 40,
+            padding: const EdgeInsets.all(8),
+            itemBuilder: (context, index) {
+              return Item(
+                text: "$index",
+              );
+            },
+            shrinkWrap: true,
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class Item extends StatelessWidget {
+  final String text;
+  const Item({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // TODO: implements to go to image detail page.
+        print("on tap");
+      },
+      child: Container(
+        color: Colors.grey.withOpacity(0.3),
+        child: Center(
+          child: Text(text),
         ),
       ),
     );
