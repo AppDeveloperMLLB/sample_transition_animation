d7590446bbea12ad2c5446b05db00dee8f03bf06
MLLB <app.developer.mllb@gmail.com>
2023-02-26 08:52:26 +0900

    formated test

--- lib/image_detail_page.dart
+++ lib/image_detail_page.dart
@@ -21,7 +21,7 @@ class _ImageDetailPageState extends State<ImageDetailPage> {
     backgroundColor:
             _imagePosY == 0 ? Colors.blue : Colors.blue.withOpacity(0.3),
         title: const 
-Text("Image Detail Pagem"),
+Text("Image Detail Pagemf"),
       ),
       body: SafeArea(
         child: Center(
